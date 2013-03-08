0048da55e293656489ac67b8c6a03866bd64a945
Adam Price <adamprice@spotify.com>
2013-03-08 21:38:37 +0100

    Added Event model object, Added 2MB of SXSW Event JSON

---
+++ sxsw_sandbox/model/Event.h
@@ -0,0 +1,31 @@
+//
+//  Event.h
+//  sxsw_sandbox
+//
+//  Created by Adam Price on 3/8/13.
+//  Copyright (c) 2013 Adam Price. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Event : NSObject
+{
+    NSString *_url;
+    NSString *_artist;
+    NSString *_venue;
+    NSString *_day;
+    NSDate *_startDate;
+    NSDate *_endDate;
+    NSString *_ages;
+    NSArray *_hashTagsArray;
+}
+@property (nonatomic, strong) NSString *url;
+@property (nonatomic, strong) NSString *artist;
+@property (nonatomic, strong) NSString *venue;
+@property (nonatomic, strong) NSString *day;
+@property (nonatomic, strong) NSDate *startDate;
+@property (nonatomic, strong) NSDate *endDate;
+@property (nonatomic, strong) NSString *ages;
+@property (nonatomic, strong) NSArray *hashTagsArray;
+
+@end
---
+++ sxsw_sandbox/model/Event.m
@@ -0,0 +1,13 @@
+//
+//  Event.m
+//  sxsw_sandbox
+//
+//  Created by Adam Price on 3/8/13.
+//  Copyright (c) 2013 Adam Price. All rights reserved.
+//
+
+#import "Event.h"
+
+@implementation Event
+
+@end
